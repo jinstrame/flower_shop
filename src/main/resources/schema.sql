06d78b0809b2d32182806beb94e9074190d35544
Evgenii Baibakov <evgenii_baibakov@epam.com>
2017-06-17 16:48:16 +0300

    0.1.8 flowers added db refactored

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -17,13 +17,22 @@ CREATE TABLE administrators (
   passwordHash NVARCHAR(300)
 );
 
+CREATE TABLE transport (
+  id INT PRIMARY KEY,
+  type ENUM('courier', 'shop'),
+  price INT
+);
+
 CREATE TABLE orders (
   id INT AUTO_INCREMENT PRIMARY KEY ,
   clientId INT,
+  onlinePayment BOOLEAN,
+  transport INT,
   time DATETIME,
   status ENUM('unpaid', 'active', 'completed'),
 
-  CONSTRAINT ORDERS_CLIENT_ID_FK FOREIGN KEY (clientId) REFERENCES clients(phone)
+  CONSTRAINT ORDERS_CLIENT_ID_FK FOREIGN KEY (clientId) REFERENCES clients(phone),
+  CONSTRAINT ORDERS_TRANSPORT_TYPE_FK FOREIGN KEY (transport) REFERENCES transport(id)
 );
 
 CREATE TABLE orderDetails (
